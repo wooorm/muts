f809a9fcb538edcf1ffb00b5e9174dde9e782cc4
wooorm <tituswormer@gmail.com>
2012-12-14 08:57:40 +0100

    Extended 'app.game' to display game results.

--- app.game/script/app.game.js
+++ app.game/script/app.game.js
@@ -38,10 +38,26 @@
 			}
 		);
 
+		var ResultView = Backbone.View.extend(
+			{
+				  'tagName' : 'tr'
+				, 'template' : function template( obj )
+				{
+					return '<td' + ( obj.winner === 'team1'?  ' class="winner"' : '' ) + '>' + obj.team1 + '</td><td>' + obj.team1Score + ' - ' + obj.team2Score + '</td><td' + ( obj.winner === 'team2'? ' class="winner"' : '' ) + '>' + obj.team2 + '</td>';
+				}
+				, 'render' : function render()
+				{
+					this.el.innerHTML = this.template( this.model );
+					return this;
+				}
+			}
+		);
 		// Create new `GameView` view.
 		var GameView = Backbone.View.extend(
 			{
-				  'el' : document.querySelectorAll( '.set_list' )
+				  'el' : document.querySelector( '#data' )
+				, 'set_el' : document.querySelector( '.set_list' )
+				, 'result_el' : document.querySelector( '.result_list' )
 				, 'initialize' : function initialize()
 				{
 					this.collection = new Game( sets );
@@ -51,12 +67,51 @@
 				{
 					var _this = this;
 
+					this.renderResults();
+
 					_.each( this.collection.models, function callback( item )
 						{
 							_this.renderSet.call( _this, item );
 						}
 						, this
 					);
+				}
+				, 'renderResults' : function renderSet()
+				{
+					var resultView = new ResultView(
+						{
+							'model' : this.cleanResults( this.collection.models )
+						}
+					);
+
+					this.result_el.appendChild( resultView.render().el );
+				}
+				, 'cleanResults' : function cleanResults( _model )
+				{
+					var attrs, model = {
+					  	    'winner'     : null
+					  	  , 'team1'      : ''
+					  	  , 'team2'      : ''
+					  	  , 'team1Score' : 0
+					  	  , 'team2Score' : 0
+					  };
+
+					_model.forEach( function callback( it, n, us )
+						{
+							attrs = it.attributes;
+
+							if ( attrs.team2Score !== attrs.team1Score )
+								model[ attrs.team2Score > attrs.team1Score? 'team2Score' : 'team1Score' ]++;
+						}
+					);
+
+					model.team1 = _model[ 0 ].attributes.team1;
+					model.team2 = _model[ 0 ].attributes.team2;
+
+					if ( model.team2Score !== model.team1Score )
+						model[ 'winner' ] = attrs.team2Score > attrs.team1Score? 'team2' : 'team1';
+
+					return model;
 				}
 				, 'renderSet' : function renderSet( item )
 				{
@@ -66,13 +121,13 @@
 						}
 					);
 
-					this.el.appendChild( setView.render().el );
+					this.set_el.appendChild( setView.render().el );
 				}
 			}
 		);
 
 		// Instanciate the `GameView` view.
-		var directory = new GameView();
+		new GameView();
 
 	}
 ( jQuery ) );
